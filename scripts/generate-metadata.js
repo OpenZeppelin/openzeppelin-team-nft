df4beeafe16a9626c9f78b5b01c120c61c7f5242
Andrew B Coathup <andrew@coathup.net>
2021-03-25 19:25:32 +1100

    update metadata

--- scripts/generate-metadata.js
+++ scripts/generate-metadata.js
@@ -1,24 +1,20 @@
 // scripts/generateMetaData.js
 async function main () {
-  const fs = require('fs');
+  const fs = require('fs').promises;
 
   const repository = 'https://github.com/OpenZeppelin/openzeppelin-team-nft/';
 
   for (let tokenId = 0; tokenId < 50; tokenId++) {
     /* eslint-disable camelcase */
     // Add token.
-    const jsonMetaData = {
-      id: tokenId,
+    let jsonMetaData = {
       description: 'OpenZeppelin Virtual Retreat, March 2021 - Commemorative tee-shirt',
       external_url: 'https://openzeppelin.com',
       image: repository + 'raw/main/images/virtual-retreat-tshirt-march-2021.png',
       name: 'Virtual Retreat Tee, March 2021',
     };
 
-    fs.writeFile('api/token/' + tokenId.toString(), jsonMetaData, function (err) {
-      if (err) console.log(err);
-      console.log('Generated JSON');
-    });
+    await fs.writeFile('api/token/' + tokenId.toString(), JSON.stringify(jsonMetaData));
   }
 }
 
